ab212fc1eb98f457445096eb28e0769a43e2a2d0
Dinesh Parimi <dineshp@berkeley.edu>
2024-12-03 07:25:05 -0800

    rework

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,22 +1,31 @@
 use std::fs;
 use regex::Regex;
 
+fn extract_muls(text_segments: Vec<&str>) -> i64 {
+    let re = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
+
+    let mut total: i64 = 0;
+    for text in text_segments {
+        let captures = re.captures_iter(text);
+        for c in captures {
+            assert_eq!(c.len(), 3);
+
+            let op1: i64 = c[1].parse().unwrap();
+            let op2: i64 = c[2].parse().unwrap();
+            total += op1 * op2;
+        }
+    }
+    total
+}
+
 fn main() {
     let filename = "dat/input.txt";
 
     let text: &str = &fs::read_to_string(filename).unwrap()[..];
+    /* TODO: part 2 */
+    let text_segments: Vec<&str> = vec![text];
 
-    let re = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
-    let captures = re.captures_iter(text);
-
-    let mut total: i64 = 0;
-    for c in captures {
-        assert_eq!(c.len(), 3);
-
-        let op1: i64 = c[1].parse().unwrap();
-        let op2: i64 = c[2].parse().unwrap();
-        total += op1 * op2;
-    }
+    let total: i64 = extract_muls(text_segments);
 
     println!("{}", total);
 }
