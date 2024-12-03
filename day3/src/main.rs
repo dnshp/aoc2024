e8b5eaee85b9d3b143d09ea1d0c3514f93fdc7df
Dinesh Parimi <dineshp@berkeley.edu>
2024-12-03 08:08:32 -0800

    part 2

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,5 +1,7 @@
 use std::fs;
 use regex::Regex;
+use std::error::Error;
+use aho_corasick::{AhoCorasick, PatternID};
 
 fn extract_muls(text_segments: Vec<&str>) -> i64 {
     let re = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
@@ -18,12 +20,37 @@ fn extract_muls(text_segments: Vec<&str>) -> i64 {
     total
 }
 
+fn get_valid_segments(text: &str) -> Result<Vec<&str>, Box<dyn Error>> {
+    let patterns = &["do()", "don't()"];
+    let ac = AhoCorasick::new(patterns)?;
+    let matches = ac.find_iter(text);
+
+    let mut valid_segments: Vec<&str> = Vec::new();
+    let mut in_valid_segment = true;
+    let mut offset = 0;
+
+    for m in matches {
+        if in_valid_segment && m.pattern() == PatternID::must(1) {
+            valid_segments.push(&text[offset..m.end()]);
+            offset = m.end();
+            in_valid_segment = false;
+        } else if !in_valid_segment && m.pattern() == PatternID::must(0) {
+            offset = m.end();
+            in_valid_segment = true;
+        }
+    }
+    if in_valid_segment {
+        valid_segments.push(&text[offset..]);
+    }
+    Ok(valid_segments)
+}
+
 fn main() {
     let filename = "dat/input.txt";
 
     let text: &str = &fs::read_to_string(filename).unwrap()[..];
-    /* TODO: part 2 */
-    let text_segments: Vec<&str> = vec![text];
+    // let text_segments: Vec<&str> = vec![text]; /* Part 1 */
+    let text_segments = get_valid_segments(text).unwrap(); /* Part 2 */
 
     let total: i64 = extract_muls(text_segments);
 
