7426ad26c4c09ae7dde8b6df384cb4f937db6332
Dinesh Parimi <dineshp@berkeley.edu>
2024-12-04 23:46:26 -0800

    5-1

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -30,14 +30,6 @@ fn parse(filename: &str) -> Result<(Vec<Rule>, Vec<Sequence>), Box<dyn Error>> {
     Ok((rules, sequences))
 }
 
-// fn sort_by_rules(a: u32, b: u32, &rules: Vec<Rule>) {
-//     for rule in rules {
-//         if rule.0 == a && rule.1 == b {
-// 
-//         }
-//     }
-// }
-
 fn check_rule_for_sequence(r: &Rule, s: &Sequence) -> bool {
     let first = s.iter().position(|x| *x == r.0);
     let second = s.iter().position(|x| *x == r.1);
@@ -50,18 +42,6 @@ fn check_rule_for_sequence(r: &Rule, s: &Sequence) -> bool {
 fn main() {
     let (rules, sequences) = parse("dat/input.txt").unwrap();
 
-    // // Push the first rule to the ordering, then repeatedly loop through rules to find one that
-    // // contains an already seen page number. Poor man's insertion sort.
-    // let order: Vec<String> = Vec::new();
-    // let first_rule: Rule = rules.remove(0);
-    // order.push(first_rule.0);
-    // order.push(first_rule.1);
-
-    // for rule in rules {
-    //     if order.contains(rule.0) {
-    //         if !order.contains(rule.1)
-    // }
-    
     // part 1
     let mut total = 0;
     for sequence in sequences {
