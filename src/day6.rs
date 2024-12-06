064654280d56aff4ee9ca33c8b6af42e48735010
Dinesh Parimi <dineshp@berkeley.edu>
2024-12-05 23:08:37 -0800

    6.2

--- src/day6.rs
+++ src/day6.rs
@@ -8,14 +8,17 @@ struct Area {
     row: usize,
     col: usize,
     dir: char,
+    visited_with_dir: HashSet<(usize, usize, char)>,
 }
 
+struct InfiniteLoop;
+
 impl Area {
-    fn propagate(&mut self) -> bool {
+    fn propagate(&mut self) -> Result<bool, InfiniteLoop> {
         match self.dir {
             '>' => {
                 if self.col >= self.obs_rows[0].len() - 1 {
-                    return true; // exit the area
+                    return Ok(true); // exit the area
                 } else if self.obs_rows[self.row][self.col + 1] {
                     self.dir = 'v'; // get rotated, idiot
                 } else {
@@ -24,7 +27,7 @@ impl Area {
             },
             'v' => {
                 if self.row >= self.obs_rows.len() - 1 {
-                    return true; // exit the area
+                    return Ok(true); // exit the area
                 } else if self.obs_rows[self.row + 1][self.col] {
                     self.dir = '<'; // get rotated, idiot
                 } else {
@@ -33,7 +36,7 @@ impl Area {
             },
             '<' => {
                 if self.col <= 0 {
-                    return true; // exit the area
+                    return Ok(true); // exit the area
                 } else if self.obs_rows[self.row][self.col - 1] {
                     self.dir = '^'; // get rotated, idiot
                 } else {
@@ -42,7 +45,7 @@ impl Area {
             },
             '^' => {
                 if self.row <= 0 {
-                    return true; // exit the area
+                    return Ok(true); // exit the area
                 } else if self.obs_rows[self.row - 1][self.col] {
                     self.dir = '>'; // get rotated, idiot
                 } else {
@@ -52,8 +55,12 @@ impl Area {
             _ => panic!("something wrong!"),
         };
 
+        if self.visited_with_dir.contains(&(self.row, self.col, self.dir)) {
+            return Err(InfiniteLoop);
+        }
         self.visited.insert((self.row, self.col));
-        false
+        self.visited_with_dir.insert((self.row, self.col, self.dir));
+        Ok(false)
     }
 }
 
@@ -81,18 +88,48 @@ fn parse(file: &str) -> Area {
     let rows: Vec<Vec<bool>> = rows.into_iter().map(|r| r.into_iter().map(|c| c == '#').collect()).collect();
     let mut visited = HashSet::new();
     visited.insert((row, col));
+    let mut visited_with_dir = HashSet::new();
+    visited_with_dir.insert((row, col, dir));
 
     Area {
         visited : visited,
         obs_rows : rows,
         row : row,
         col : col,
-        dir : dir
+        dir : dir,
+        visited_with_dir : visited_with_dir,
     }
 }
 
 pub fn main(file: &str) {
     let mut area: Area = parse(file);
-    while !area.propagate() {}
+    loop {
+        match area.propagate() {
+            Ok(true) => break,
+            Ok(false) => {},
+            Err(_) => panic!("unexpected infinite loop!"),
+        }
+    }
     println!("{}", area.visited.len());
+
+    let visited = area.visited.clone();
+    let mut infinite_solutions = 0;
+    for candidate in visited {
+        let mut area: Area = parse(file);
+        assert!(!area.obs_rows[candidate.0][candidate.1]);
+        area.obs_rows[candidate.0][candidate.1] = true;
+        loop {
+            match area.propagate() {
+                Ok(true) => {
+                    break;
+                },
+                Ok(false) => {},
+                Err(InfiniteLoop) => {
+                    infinite_solutions += 1;
+                    break;
+                },
+            }
+        }
+    }
+    println!("{}", infinite_solutions);
 }
