78b524dd130ab67ec6e2e0c5692ddcdd7dcfe2c1
Dinesh Parimi <dineshp@berkeley.edu>
2024-12-08 22:45:34 -0800

    refactor

--- src/day9.rs
+++ src/day9.rs
@@ -1,13 +1,7 @@
 use std::fs;
+use std::collections::HashSet;
 
-pub fn main(file: &str) {
-    let f = fs::read_to_string(file).unwrap();
-    let mut raw: Vec<_> = f.trim().chars().map(|x| x.to_digit(10).unwrap() as usize).collect();
-    // it wouldn't make a great deal of sense to show the free blocks at the end, so I'm assuming
-    // the filesystem always ends in an occupied block, but let's make sure so I don't break my
-    // assumptions later...
-    assert_eq!(raw.len() % 2, 1);
-
+fn defrag_1(mut raw: Vec<usize>) -> usize {
     let fs_size: usize = raw.clone().into_iter().step_by(2).sum();
     let mut fs_repr: Vec<usize> = Vec::new();
     fs_repr.reserve_exact(fs_size);
@@ -18,21 +12,27 @@ pub fn main(file: &str) {
     let mut b_idx = raw.len() - 1;
     let mut f_file = 0;
     let mut b_file = (raw.len() - 1) / 2;
-    while f_idx < b_idx {
+    while f_idx <= b_idx {
+        println!("{} {}", f_idx, b_idx);
         // sanity check
         assert_eq!(f_idx % 2, 0);
         assert_eq!(b_idx % 2, 0);
-        for i in 0..raw[f_idx] {
+
+        // push the occupied blocks
+        for _i in 0..raw[f_idx] {
             fs_repr.push(f_file);
         }
         f_file += 1;
         f_idx += 1;
 
-        'outer: for i in 0..raw[f_idx] {
+        // draw from the occupied blocks at the back to fill free at the front
+        'outer: for _i in 0..raw[f_idx] {
+            // current file from the back is fully defragged
             if raw[b_idx] <= 0 {
                 b_idx -= 2;
                 b_file -= 1;
             }
+            // just in case we broke our loop condition here
             if b_idx <= f_idx {
                 break 'outer;
             }
@@ -41,10 +41,33 @@ pub fn main(file: &str) {
         }
         f_idx += 1;
     }
-
     assert_eq!(fs_size, fs_repr.len());
 
     let checksum: usize = fs_repr.iter().enumerate().map(|(i, f)| i * f).sum();
+    checksum
+}
 
-    println!("{}", checksum);
+fn defrag_2(mut raw: Vec<usize>) -> usize {
+    let fs_size: usize = raw.clone().iter().sum();
+    let mut fs_repr: Vec<usize> = Vec::new();
+    fs_repr.reserve_exact(fs_size);
+
+    // aaaa
+
+    let checksum: usize = fs_repr.iter().enumerate().map(|(i, f)| i * f).sum();
+    checksum
+}
+
+pub fn main(file: &str) {
+    let f = fs::read_to_string(file).unwrap();
+    let raw: Vec<_> = f.trim().chars().map(|x| x.to_digit(10).unwrap() as usize).collect();
+    // it wouldn't make a great deal of sense to show the free blocks at the end, so I'm assuming
+    // the filesystem always ends in an occupied block, but let's make sure so I don't break my
+    // assumptions later...
+    assert_eq!(raw.len() % 2, 1);
+
+    let checksum = vec![defrag_1(raw.clone()), defrag_2(raw)];
+
+    println!("Part 1 checksum: {}", checksum[0]);
+    println!("Part 2 checksum: {}", checksum[1]);
 }
